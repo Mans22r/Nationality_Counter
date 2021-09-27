f836911341ab97c9764fdb8869d954d6668f1115
Mansoor Majeed <83159118+Mans22r@users.noreply.github.com>
2021-09-27 23:46:51 +0530

    clr

--- src/components/Country.js
+++ src/components/Country.js
@@ -6,7 +6,6 @@ let Country = (props) => {
         <span className="country-id"><img src={`https://www.countryflags.io/${props.countryId}/shiny/32.png`} alt={props.countryId}></img></span>
         <span className="percentage-bar"><progress value={props.percent*100} max="100"></progress></span>
         <span className="percentage-numeral">{`${Math.round(props.percent*100)}%`}</span>
-
     </div>
     );
 }
